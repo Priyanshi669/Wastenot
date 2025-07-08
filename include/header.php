f0e140c6cc84b73d9372fd21f61fb62a5ad0cee3
kanikajain2 <Kanikaj0210@gmail.com>
2025-07-08 19:33:37 +0530

    Create header.php

---
+++ include/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Food Donation Website</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-success" >
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Waste NOT</a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="about.php">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="donate.php">Donate</a></li>
+                    <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<!DOCTYPE html>
+<html lang="en">
