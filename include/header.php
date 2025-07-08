c1b6a4040d6c59e2a24e62fa8a0472efc92df417
kanikajain2 <Kanikaj0210@gmail.com>
2025-07-08 19:47:30 +0530

    Update header.php

--- include/header.php
+++ include/header.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <title>Food Donation Website</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-success" >
         <div class="container">
-            <a class="navbar-brand" href="index.php">Waste NOT</a>
+            <a class="navbar-brand" href="index.php" style="color: rgba(255, 255, 255, 0.95);font-size: 1.8rem; font-weight: bold;">Waste NOT</a>
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
